eb6891a746a47aba32d171ccbe36648a26439277
Yara Oliveira <magicsunshine_ya@msn.com>
2020-02-14 10:37:54 -0300

    test case 1

--- loggi/tests/unit/examples/test_identifier_ec.py
+++ loggi/tests/unit/examples/test_identifier_ec.py
@@ -1,6 +1,8 @@
 from examples.identifier import Identifier
 
-def test_test_case_01:
+def test_case_01:
     identifier = Identifier()
 
     is_valid = identifier.validate_identifier('ab')
+
+    assert is_valid is True
