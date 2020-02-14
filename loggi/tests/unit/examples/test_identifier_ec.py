57f4bec4ee6912743f4c88f539f2e75d50f2cd2c
Yara Oliveira <magicsunshine_ya@msn.com>
2020-02-14 13:20:41 -0300

    tests cases

--- loggi/tests/unit/examples/test_identifier_ec.py
+++ loggi/tests/unit/examples/test_identifier_ec.py
@@ -1,8 +1,36 @@
 from examples.identifier import Identifier
 
-def test_case_01:
+def test_case_01():
     identifier = Identifier()
 
     is_valid = identifier.validate_identifier('ab')
 
     assert is_valid is True
+
+def test_case_02():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('')
+
+    assert is_valid is False
+
+def test_case_03():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('1a')
+
+    assert is_valid is False
+
+def test_case_04():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('abcdfg')
+
+    assert is_valid is True
+
+def test_case_05():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('açai')
+
+    assert is_valid is False
