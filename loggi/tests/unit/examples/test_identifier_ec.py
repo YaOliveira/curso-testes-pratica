4a500c82aff30dbd27e4a3d4bb813da7b53f1ecb
Yara Oliveira <magicsunshine_ya@msn.com>
2020-02-14 13:52:14 -0300

    ninth test case

--- loggi/tests/unit/examples/test_identifier_ec.py
+++ loggi/tests/unit/examples/test_identifier_ec.py
@@ -3,34 +3,70 @@ from examples.identifier import Identifier
 def test_case_01():
     identifier = Identifier()
 
-    is_valid = identifier.validate_identifier('ab')
-
-    assert is_valid is True
-
-def test_case_02():
-    identifier = Identifier()
-
     is_valid = identifier.validate_identifier('')
 
     assert is_valid is False
 
+def test_case_02():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('a')
+
+    assert is_valid is True
+
 def test_case_03():
     identifier = Identifier()
 
-    is_valid = identifier.validate_identifier('1a')
+    is_valid = identifier.validate_identifier('ab')
 
-    assert is_valid is False
+    assert is_valid is True
 
 def test_case_04():
     identifier = Identifier()
 
-    is_valid = identifier.validate_identifier('abcdfg')
+    is_valid = identifier.validate_identifier('abcde')
 
     assert is_valid is True
 
 def test_case_05():
     identifier = Identifier()
 
+    is_valid = identifier.validate_identifier('abcdef')
+
+    assert is_valid is True
+
+def test_case_06():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('abcdfg')
+
+    assert is_valid is True
+
+def test_case_07():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('abcdfgh')
+
+    assert is_valid is False
+
+def test_case_07():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('1a')
+
+    assert is_valid is False
+
+def test_case_08():
+    identifier = Identifier()
+
+    is_valid = identifier.validate_identifier('a1')
+
+    assert is_valid is True
+
+def test_case_09():
+    identifier = Identifier()
+
     is_valid = identifier.validate_identifier('açai')
 
     assert is_valid is False
+
